cb5a6269b6924fa68c95167b5f7eb1f63c4b587f
Rob Thompson <rpt26@cam.ac.uk>
2014-11-28 00:17:07 +0000

    Updated to move callable functions to a utils module. I've also changed the way that an atom is defined, to be a list consisting of three cartesian coordinates and a charge in terms of the fundamental electronic charge. I.e. atom = [x,y,z,q]

--- coulomb_energy.py
+++ coulomb_energy.py
@@ -1,52 +1,26 @@
-# This file contains the various functions I will use through my attempt to calculate the coloumbic energy of a crystal using the ewald summation method.
+# This file contains the various functions I will use through my attempt to calculate the coloumbic energy of a crystal.
+# This will be done with rather brute force methods initially, possibly moving to the more elegant Ewald sum.
 
-def pair_energy(charge1, charge2, pos1, pos2):
-    energy = 0
-    import math
-
-    sep = math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2 + (pos1[2] - pos2[2]) ** 2)
-    if sep != 0:  # avoid divide by zero for an atom being paired with itself
-        energy = ( charge1 * charge2 ) / sep
-    return energy
+import coulomb_utils
 
 
-def separation(pos1, pos2):
-    import math
-
-    sep = math.sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2 + (pos1[2] - pos2[2]) ** 2)
-    return sep
-
-
-def madelung(limit):
-    # Limit is how far out I want to calculate this energy
+chargeA = 1  # Charge in terms of electronic charge
+chargeB = -1  # Again
+refA = [0, 0, 0, chargeA]  # The fixed A atom we're going to use in a pair energy withg a working atom
+refB = [0.5, 0.5, 0.5, chargeB]  # Similarly for B
 
 
-    refA = [0, 0, 0]  # The fixed A atom we're going to use in a pair energy withg a working atom
-    refB = [0.5, 0.5, 0.5]  # Similarly for B
-    chargeA = 1  # Charge in terms of electronic charge
-    chargeB = -1  # Again
-    posA = [0, 0, 0]  # The first value for the working A atom
-    posB = [0.5, 0.5, 0.5]  # Similarly for B
-    energy = 0  # Initial value for energy
+reference_atoms = [refA, refB]
+energy = 0
 
-    for z in range(limit):  # stepping out in z, starting with zero deliberately to catch the atoms in the unit cell
-        posA[2] += z
-        posB[2] += z
-        for y in range(limit):  # Stepping outward in y
-            posA[1] += y
-            posB[1] += y
-            for x in range(limit): # STepping outward in x
-                posA[0] += x
-                posB[0] += x
-                # All of the available pairs between reference atoms and working atoms.
-                # Need to consider double counting formally
-                energy += pair_energy(chargeA, chargeA, refA, posA)
+file = open('convergence.txt', 'a')
+for number in range(500):
+    energy += coulomb_utils.madelung_sum(reference_atoms, number)
+    string = str(number) + ' ' + str(energy)
+    file.write(string + '\n')
+    print(number, energy)
 
-                energy += pair_energy(chargeB, chargeA, refA, posB)
-
-                energy += pair_energy(chargeB, chargeB, refB, posB)
-
-                energy += pair_energy(chargeA, chargeB, refB, posA)
+file.close()
 
 
-    return energy
+
---
+++ coulomb_utils.py
@@ -0,0 +1,39 @@
+__author__ = 'Rob'
+
+def atom_pair_energy(atom1, atom2):
+    energy = 0
+    import math
+    sep = math.sqrt((atom1[0] - atom2[0]) ** 2 + (atom1[1] - atom2[1]) ** 2 + (atom1[2] - atom2[2]) ** 2)
+    if sep != 0:  # avoid divide by zero for an atom being paired with itself
+        energy = (atom1[3] * atom2[3]) / sep
+    return energy
+
+def unit_cell_pair_energy(atom_list1, atom_list2):
+    energy = 0
+    for atom1 in atom_list1:
+        for atom2 in atom_list2:
+            energy += atom_pair_energy(atom1, atom2)
+    return energy
+
+
+def separation(atom1, atom2):
+    import math
+    sep = math.sqrt((atom1[0] - atom2[0]) ** 2 + (atom1[1] - atom2[1]) ** 2 + (atom1[2] - atom2[2]) ** 2)
+    return sep
+
+def madelung_sum(atoms_in_unit_cell, limit):
+    current_atoms = []
+    for atom in atoms_in_unit_cell:
+        current_atoms.append(atom[:])
+    energy = 0  # Initial value for energy
+    for z in range(-limit, limit):  # stepping across z
+        for atom in current_atoms:
+            atom[2] += z
+        for y in range(-limit,limit):  # Stepping across y
+            for atom in current_atoms:
+                atom[1] += y
+            for x in range(-limit,limit):  # Stepping across x
+                for atom in current_atoms:
+                    atom[0] += x
+                energy += unit_cell_pair_energy(current_atoms, atoms_in_unit_cell)
+    return energy
